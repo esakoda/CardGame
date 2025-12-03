838bf3826ed7e7b861db074673736ac91f226843
ellensakoda <ellen.sakoda@menloschool.org>
2025-12-02 23:27:56 -0800

    Completed playRound function in Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -17,6 +17,10 @@ public class Game {
     private Player current;
     private Card choice;
     private Card cardFromHand;
+    private boolean heartsBroken;
+    private String ledSuit;
+    private String[] ranks;
+    private Card takerCard;
 
     public Game(){
         // Get the name of each player
@@ -28,6 +32,7 @@ public class Game {
         }
         // Create main deck of cards
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        this.ranks = ranks;
         String[] suits = {"Diamonds", "Clubs", "Spades", "Hearts"};
         int[] values = {0, 1, 13};
         deck = new Deck(ranks, suits, values);
@@ -58,6 +63,9 @@ public class Game {
 
         // Create trick pile
         trick = new ArrayList<>();
+
+        // Hearts have not been broken yet
+        heartsBroken = false;
     }
 
     public void printInstructions(){
@@ -118,8 +126,162 @@ public class Game {
         return null;
     }
 
-    public void playRound(){
+    // Get the value of each rank, so I can compare them (2 is the lowest, ace is the highest)
+    public int getRankValue(String rank){
+        for (int i = 0; i < ranks.length; i++){
+            if (ranks[i].equals(rank)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    // Play one trick (all 4 players place down cards)
+    public void playRound(){
+        // The player who won the last trick leads the next trick
+        Player leader = taker;
+        current = leader;
+
+        // Play 4 cards
+        for (int i = 0; i < 4; i++){
+            System.out.println("\n" + current.getName() + "'s turn:");
+            System.out.println(current.toString());
+
+            Scanner scanner = new Scanner(System.in);
+            boolean validPlay = false;
+
+            // If the player inputs an invalid card
+            while(!validPlay){
+                System.out.println("Pick your card (format: rank suit):");
+                String userInput = scanner.nextLine().trim();
+                int spaceIndex = userInput.indexOf(" ");
+
+                // If the user formats their input wrong
+                if (spaceIndex == -1){
+                    System.out.println("Invalid format! (format: rank suit)");
+                    // Prevents error of Java trying to split a string with no space
+                    continue;
+                }
+
+                // Take user input and get the rank
+                String rank = userInput.substring(0, spaceIndex);
+                // Take user input and get the suit
+                String suit = userInput.substring(spaceIndex + 1);
+
+                // Create the card they want to play
+                choice = new Card(suit, rank);
+                int index = current.getHand().indexOf(choice);
+
+                // If the user doesn't have the card they wanted to place
+                if (index == -1){
+                    System.out.println("You don't have that card!");
+                    // Take them back to the beginning of the loop so they input another card
+                    continue;
+                }
+
+                // Get the actual card from their hand (has points)
+                cardFromHand = current.getHand().get(index);
+
+                // Check if it's a valid play
+                if (i == 0){
+                    // First player of the trick (leader)
+                    // Can't lead hearts unless hearts have been broken
+                    if (cardFromHand.getSuit().equals("Hearts") && !heartsBroken){
+                        // Check if they have any non hearts they can lead with
+                        boolean hasNonHearts = false;
+                        for (int j = 0; j < current.getHand().size(); j++){
+                            if (!current.getHand().get(j).getSuit().equals("Hearts")){
+                                hasNonHearts = true;
+                                break;
+                            }
+                        }
+                        if (hasNonHearts){
+                            System.out.println("You can't lead hearts until hearts are broken!");
+                            continue;
+                        }
+                        // If they only have hearts, then they can play hearts
+                    }
+                    //
+                    ledSuit = cardFromHand.getSuit();
+                    validPlay = true;
+                }
+                else {
+                    // Not the first player
+                    // Must follow the suit the leader picked, if possible
+                    if (cardFromHand.getSuit().equals(ledSuit)){
+                        validPlay = true;
+                    }
+                    else {
+                        // Check if they have any cards of the led suit
+                        boolean hasLedSuit = false;
+                        for (int j = 0; j < current.getHand().size(); j++){
+                            if (current.getHand().get(j).getSuit().equals(ledSuit)){
+                                hasLedSuit = true;
+                                break;
+                            }
+                        }
+                        if (hasLedSuit){
+                            System.out.println("You must place the same suit as the first card placed if you can!");
+                            continue;
+                        }
+                        else {
+                            // If they don't have the led suit, they can play anything
+                            // Check if they played a heart because if so, hearts are now broken
+                            if (cardFromHand.getSuit().equals("Hearts")){
+                                heartsBroken = true;
+                            }
+                            validPlay = true;
+                        }
+                    }
+                }
+                // If valid play, remove hand and add to trick
+                if (validPlay){
+                    current.getHand().remove(index);
+                    trick.add(cardFromHand);
+                    System.out.println(current.getName() + " plays " + cardFromHand);
+
+                    // Check if this player wins the trick
+                    if ( i == 0){
+                        // First player will take trick if no one places higher
+                        taker = current;
+                        takerCard = cardFromHand;
+                    }
+                    else {
+                        // If another player places a card that is the same suit as led suit and is a higher rank
+                        // They become the taker
+                        if(cardFromHand.getSuit().equals(ledSuit)){
+                            if (getRankValue(cardFromHand.getRank()) > getRankValue(takerCard.getRank())){
+                                taker = current;
+                                takerCard = cardFromHand;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Move to next player
+            current = nextPlayer(current);
+        }
+
+        // After all 4 players have played, taker takes the trick
+        System.out.println("\n" + taker.getName() + " takes the trick!");
+
+        // Add up points from this trick
+        int trickPoints = 0;
+        for (int i = 0; i <trick.size(); i++) {
+            trickPoints += trick.get(i).getValue();
+        }
+
+        // Give the points from the trick to the taker
+        if (trickPoints > 0) {
+            System.out.println(taker.getName() + " takes " + trickPoints + " points.");
+            taker.addPoints(trickPoints);
+        }
+
+        // Clear the trick for next round
+        while (trick.size() > 0) {
+            trick.remove(0);
+        }
     }
 
     public void playGame(){
@@ -129,25 +291,6 @@ public class Game {
         trick.add(twoOfClubs);
         taker = playerWithTwoOfClubs();
         current = nextPlayer(playerWithTwoOfClubs());
-        // Next player sees hand and then plays
-        System.out.println(current.toString());
-        System.out.println("Pick your card (reminder: you have to play the same suit if you can!): ");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        int spaceIndex = userInput.indexOf(" ");
-        // Take user input and get the rank
-        String rank = userInput.substring(0,spaceIndex);
-        // Take user input and get the suit
-        String suit = userInput.substring(spaceIndex);
-        // Take the card they inputted and remove it from their hand and add to trick pile
-        choice = new Card(suit, rank);
-        int index = current.getHand().indexOf(choice);
-        if (index != -1){
-            Card cardFomHand = current.getHand().remove(index);
-            // Notes: check if it's the same suit as previous suit played and if rank is higher
-            // If it is make this player the taker
-            trick.add(cardFomHand);
-        }
     }
 
     public static void main(String[] args) {
